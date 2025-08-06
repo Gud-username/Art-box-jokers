e9b548d4e55b3d42cf0ca061c0dac859b6b9a013
Lusty joker <toby.hayter10@gmail.com>
2025-08-06 12:26:32 +0100

    Update credits.lua

--- credits.lua
+++ credits.lua
@@ -1,7 +1,7 @@
 SMODS.current_mod.extra_tabs = function()
     local credits_tab = {
       n = G.UIT.ROOT,
-      config = { align = "tl", padding = 0.05, emboss = 0.05, r = 0.1, colour = G.C.BLACK },
+      config = { align = 'tl', padding = 0.05, emboss = 0.05, r = 0.1, colour = G.C.BLACK },
       nodes = { {
         n = G.UIT.R,
         nodes = {
@@ -17,9 +17,9 @@ SMODS.current_mod.extra_tabs = function()
                   },
                   {
                     n = G.UIT.R,
-                    config = { align = "cm", minh = 0.25 },
+                    config = { align = 'cm', minh = 0.25 },
                     nodes = {
-                      { n = G.UIT.T, config = { text = "Smol", colour = G.C.IMPORTANT, scale = 0.4 } }
+                      { n = G.UIT.T, config = { text = "Smol (kinda)", colour = G.C.PURPLE, scale = 0.4 } }
                     }
                   },
                 }
@@ -31,26 +31,26 @@ SMODS.current_mod.extra_tabs = function()
               {
                 n = G.UIT.R,
                 nodes = {
-                  { n = G.UIT.T, config = { text = "Contributors", colour = G.C.MULT, scale = 0.75 } },
+                  { n = G.UIT.T, config = { text = "Artists", colour = G.C.CHIPS, scale = 0.75 } },
                 }
               },
               {
                 n = G.UIT.R,
-                config = { align = "cm", minh = 0.25 },
+                config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "SadCube", colour = G.C.PURPLE, scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "SadCube", colour = G.C.GREEN, scale = 0.4 } }
                 }
               },
               {
                 n = G.UIT.R,
-                config = { align = "cm", minh = 0.25 },
+                config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "Samuran", colour = G.C.CHIPS, scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "Samuran", colour = G.C.GREEN, scale = 0.4 } }
                 }
               },
               {
                 n = G.UIT.R,
-                config = { align = "cm", minh = 0.25 },
+                config = { align = 'cm', minh = 0.25 },
                 nodes = {
                   { n = G.UIT.T, config = { text = "Superb", colour = G.C.GREEN, scale = 0.4 } }
                 }
