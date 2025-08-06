bb447ca9d55315740fabdd35e448a50cefe0cbe8
Lusty joker <toby.hayter10@gmail.com>
2025-08-06 12:28:38 +0100

    Create default.lua

---
+++ localisation/default.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        texture_packs = {
+            texpack_art_box_jokers = {
+                name = "Artboxjokers",
+                text = {
+                    "Adds the option to make all jokers look like colectables from art box",,
+                }
+            }
+        }
+    }
+}
